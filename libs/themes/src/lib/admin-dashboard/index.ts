99169346c02d29259653df6b4bc7864dab9ad4a6
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-02-19 17:14:10 +0100

    feat: setup-styled-components-theme

---
+++ libs/themes/src/lib/admin-dashboard/index.ts
@@ -0,0 +1,6 @@
+// Do not edit; to change theme edit theme.ts
+
+import { DefaultTheme } from "styled-components";
+import theme from "./theme";
+
+export const dashboardTheme: DefaultTheme = theme;
---
+++ libs/themes/src/lib/admin-dashboard/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+    font: "Outfit",
+
+    colors: {
+        primary: {
+            "100": "#F1F1FD",
+            "200": "#E5E5FF",
+            "300": "#A4A4DA",
+            "400": "#7879F1",
+            "500": "#67678E",
+            "600": "#2C2C81",
+            "700": "#09095D",
+            shadow: {
+                light: "#402C2C81",
+                strong: "#802C2C81",
+            },
+        },
+        background: "#ffffff",
+    },
+};
+
+export default theme;

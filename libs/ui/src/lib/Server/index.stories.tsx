a7794dd031c6ae78f463d4d2891a294ca7587dff
mattponiat <pinkyball50@gmail.com>
2022-04-05 17:02:43 +0200

    feat: add Server component

---
+++ libs/ui/src/lib/Server/index.stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from "@storybook/react";
+import { Server, ServerProps } from "./index";
+
+export default {
+    component: Server,
+    title: "Server",
+} as Meta;
+
+export const Template: Story<ServerProps> = (args) => <Server {...args} />;
+
+Template.args = {
+    imageSrc:
+        "https://cdn.discordapp.com/avatars/380454126364131332/1476ffee61d845bbe5a1027da9cb8db3.webp",
+    serverName: "Test Server",
+    href: "",
+};

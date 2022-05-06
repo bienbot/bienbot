451d5140aa8eb60e00f464599775b4fb3fa15957
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-06 23:34:27 +0200

    feat: :sparkles: Add LeaderboardPanel component

---
+++ apps/admin-dashboard/components/LeaderboardPanel/leaderboardPanel.tsx
@@ -0,0 +1,42 @@
+import {
+    getMostActiveTextUsers,
+    getMostActiveVoiceUsers,
+} from "@bienbot/functions";
+import { GuildData } from "@bienbot/types";
+import { UserLeaderboard } from "@bienbot/ui";
+import * as React from "react";
+import styled from "styled-components";
+
+const LeaderboardPanel = ({ guildData }: { guildData: GuildData }) => {
+    return (
+        <StyledWrapper>
+            <UserLeaderboard
+                text="hours"
+                heading="Voice channels"
+                users={getMostActiveVoiceUsers(
+                    guildData.channelStats,
+                    guildData.users,
+                    5
+                )}
+            />
+            <UserLeaderboard
+                text="messages"
+                heading="Text channels"
+                users={getMostActiveTextUsers(
+                    guildData.messages,
+                    guildData.users,
+                    5
+                )}
+            />
+        </StyledWrapper>
+    );
+};
+
+const StyledWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(min(100%, 420px), 1fr));
+    gap: 16px;
+    grid-column: 1/2;
+`;
+
+export { LeaderboardPanel };

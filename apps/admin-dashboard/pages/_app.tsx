b8bd7f70aa1c7a86e7a10bf82a8bc677aa0ca5ce
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-07 10:36:11 +0200

    feat: :lipstick: Add fonts, theme provider and remove style.css file

--- apps/admin-dashboard/pages/_app.tsx
+++ apps/admin-dashboard/pages/_app.tsx
@@ -1,16 +1,27 @@
+import { dashboardTheme } from "@bienbot/themes";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import "./styles.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    /* padding: 0;
+    margin: 0; */
+}`;
 
 function CustomApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <GlobalStyles />
             <Head>
                 <title>Welcome to admin-dashboard!</title>
             </Head>
-            <main className="app">
-                <Component {...pageProps} />
-            </main>
+            <ThemeProvider theme={dashboardTheme}>
+                <main className="app">
+                    <Component {...pageProps} />
+                </main>
+            </ThemeProvider>
         </>
     );
 }

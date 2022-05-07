3e9eab2f05225e100824c6095142320711e21526
mattponiat <pinkyball50@gmail.com>
2022-05-07 20:08:35 +0200

    refactor: :recycle: Add custom storybook controls

--- libs/ui/src/lib/SearchResult/index.stories.tsx
+++ libs/ui/src/lib/SearchResult/index.stories.tsx
@@ -5,20 +5,33 @@ import { GoMail } from "react-icons/go";
 export default {
     component: SearchResult,
     title: "SearchResult",
+    argTypes: {
+        icon: {
+            options: ["icon", "image"],
+            control: {
+                type: "select",
+            },
+        },
+    },
 } as Meta;
 
-export const Template: Story<SearchResultProps> = (args) => (
-    <SearchResult {...args} />
-);
+export const Template: Story<SearchResultProps> = (args) => {
+    const icon =
+        args.icon === "icon" ? (
+            <GoMail />
+        ) : (
+            <img
+                src="https://via.placeholder.com/32x32"
+                width={32}
+                height={32}
+                alt=""
+            />
+        );
+
+    return <SearchResult {...args} icon={icon} />;
+};
 
 Template.args = {
-    icon: (
-        <img
-            width={32}
-            height={32}
-            src="https://avatars.githubusercontent.com/u/61357400?v=4"
-        />
-    ),
     children: "",
     href: "",
 };

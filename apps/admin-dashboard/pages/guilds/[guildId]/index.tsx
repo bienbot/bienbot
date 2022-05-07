5e2f44b01860dce405b8547743b8f5b0df5703e6
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-07 22:59:59 +0200

    refactor: :recycle: Use DashboardLayout in Guild Dashbard page

--- apps/admin-dashboard/pages/guilds/[guildId]/index.tsx
+++ apps/admin-dashboard/pages/guilds/[guildId]/index.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import type { NextPage } from "next";
-import { TopBar } from "@bienbot/ui";
 import {
     fetchGuildData,
     getGuildMessages,
@@ -8,12 +6,14 @@ import {
 } from "@bienbot/functions";
 import { GuildData } from "@bienbot/types";
 import { GuildDashboard } from "apps/admin-dashboard/components/GuildDashboard";
+import firebaseApp from "apps/admin-dashboard/services/firebase";
+import DashboardLayout from "apps/admin-dashboard/components/DashboardLayout/dashboardLayout";
 
 type Props = {
     guildData: GuildData;
 };
 
-const GuildDashboardPage: NextPage<Props> = ({ guildData }) => {
+const GuildDashboardPage = ({ guildData }: Props) => {
     const statistics = React.useMemo(() => {
         return getServerStatistics(guildData);
     }, []);
@@ -21,17 +21,27 @@ const GuildDashboardPage: NextPage<Props> = ({ guildData }) => {
 
     return (
         <>
-            <TopBar serverName={guildData.data.name ?? ""} />
             <GuildDashboard guildData={guildData} statistics={statistics} />
         </>
     );
 };
 
 export async function getServerSideProps({ params }) {
-    const guildData = await fetchGuildData(params.guildId);
+    const guildData = await fetchGuildData(params.guildId, firebaseApp);
 
     // Pass data to the page via props
     return { props: { guildData } };
 }
+
+GuildDashboardPage.getLayout = function getLayout(page: React.ReactElement) {
+    const { props } = page;
+    return (
+        <>
+            <DashboardLayout guildData={props.guildData}>
+                {page}
+            </DashboardLayout>
+        </>
+    );
+};
 
 export default GuildDashboardPage;

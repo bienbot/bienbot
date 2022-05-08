efcc3fb740938309b25fa3230f35c998fd39e9e5
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-08 14:49:33 +0200

    refactor: :fire: Remove event logging on messageCreate

--- apps/bot/src/events/message/messageCreate.ts
+++ apps/bot/src/events/message/messageCreate.ts
@@ -2,7 +2,6 @@ import BaseEvent from "../../utils/structures/BaseEvent";
 import { Message } from "discord.js";
 import DiscordClient from "../../client/client";
 import addMessage from "../../utils/function/addMessage";
-import logEvent from "../../utils/function/logEvent";
 
 class MessageEvent extends BaseEvent {
     constructor() {
@@ -16,36 +15,8 @@ class MessageEvent extends BaseEvent {
             }: ${message.content}`
         );
         if (message.author.bot) return;
-        if (message.content.startsWith(client.prefix)) {
-            const [cmdName, ...cmdArgs] = message.content
-                .slice(client.prefix.length)
-                .trim()
-                .split(/\s+/);
-            const command = client.commands.get(cmdName);
-            if (command) {
-                command.run(client, message, cmdArgs);
-            }
-        }
-        if (
-            message.content.toLowerCase().endsWith(" co") ||
-            message.content.toLowerCase().endsWith(" co?") ||
-            message.content.toLowerCase().endsWith(" co!") ||
-            message.content.toLowerCase().startsWith("co")
-        ) {
-            // await message.channel.send("jajco 1:0");
-            await message.reply("jajco 1:0");
-        }
+
         addMessage(message, client);
-        logEvent({
-            eventDescription: "Sent message",
-            eventTarget: message.content,
-            eventMember: message.author,
-            eventTargetId: message.id,
-            eventTime: new Date(),
-            guildId: message.guildId,
-            eventType: "messageCreate",
-            client,
-        });
     }
 }
 

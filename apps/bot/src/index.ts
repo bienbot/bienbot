c3c0d59e8b107ccd67acbecf51b04e272e260506
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-09 12:00:43 +0200

    refactor: :recycle: Move updateUsersData to setInterval

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -43,13 +43,16 @@ admin.initializeApp({
     await registerCommands(client, "../commands");
     await registerEvents(client, "../events");
     await client.login(process.env["TOKEN"]);
-    updateUsersData(client);
     /* 
     Update users in voice channels every hour; This is not ideal, beacuse we loose "accuracy"
     but current database (firestore) gets really slow when we have a lot of entries.
     In the future we might switch to a Redis database, and update the users every minute.
     */
     setInterval(() => updateUsersInVoice(client), 1000 * 60 * 60);
-    /* */
-    setInterval(() => updateGuildData(client), 1000 * 60);
+
+    /* Update server data every minute */
+    setInterval(async () => {
+        await updateGuildData(client);
+        await updateUsersData(client);
+    }, 1000 * 60);
 })();

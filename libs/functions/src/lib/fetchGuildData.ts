7600dee114cff68f2c5574d4f0c61ad94cf5f7e8
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-21 00:29:26 +0000

    refactor: :recycle: Make fetchGuildData work with subcollections

--- libs/functions/src/lib/fetchGuildData.ts
+++ libs/functions/src/lib/fetchGuildData.ts
@@ -1,4 +1,4 @@
-import { GuildData } from "@bienbot/types";
+import { GuildData, MessageData } from "@bienbot/types";
 import { getDocs, collection, getFirestore } from "firebase/firestore";
 import { FirebaseApp } from "firebase/app";
 
@@ -11,6 +11,31 @@ const fetchGuildData = async (guildId: string, firebaseApp: FirebaseApp) => {
     });
 
     const guildData: GuildData = JSON.parse(JSON.stringify(result));
+
+    // Get messages from subcollections
+
+    let messagesData: Record<string, MessageData[]> = {};
+    const messagesChannelsList = Object.keys(guildData.messages);
+
+    for await (const channelId of messagesChannelsList) {
+        const channelRef = await getDocs(
+            collection(database, guildId, "messages", channelId)
+        );
+
+        channelRef.forEach((message) => {
+            const messageData: MessageData = message.data() as MessageData;
+            if (!messagesData[channelId]) {
+                messagesData[channelId] = [messageData];
+            } else {
+                messagesData[channelId].push(messageData);
+            }
+        });
+    }
+
+    // Merge messagesData into guildData
+
+    guildData.messages = JSON.parse(JSON.stringify(messagesData));
+
     return guildData;
 };
 

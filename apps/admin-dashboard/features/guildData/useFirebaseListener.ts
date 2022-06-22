c436aac15d840619f88f4de5641cd36a04818b0e
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-22 12:11:09 +0000

    refactor: :recycle: Make useFirebaseListener handle undefined id

--- apps/admin-dashboard/features/guildData/useFirebaseListener.ts
+++ apps/admin-dashboard/features/guildData/useFirebaseListener.ts
@@ -8,7 +8,6 @@ import {
 import * as React from "react";
 import { useDispatch } from "react-redux";
 import firebaseApp from "../../services/firebase";
-import { emptyGuildData } from "./emptyGuildDataObject";
 import { updateData, updateGuild } from "./guildDataSlice";
 
 const database = getFirestore(firebaseApp);
@@ -19,71 +18,79 @@ const database = getFirestore(firebaseApp);
  * @param guildId The guild id to listen to.
  */
 const useFirebaseListener = ({ id }: { id: string }) => {
-    const q = query(collection(database, id));
     const dispatch = useDispatch();
 
     // Update guild data when it changes
     React.useEffect(() => {
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-            const data = {};
+        if (id) {
+            const q = query(collection(database, id));
+            const unsubscribe = onSnapshot(q, (snapshot) => {
+                const data = {};
 
-            snapshot.docs.forEach((doc) => {
-                if (doc.id !== "data") {
-                    data[doc.id] = doc.data();
-                }
+                snapshot.docs.forEach((doc) => {
+                    if (doc.id !== "data") {
+                        data[doc.id] = doc.data();
+                    }
+                });
+                dispatch(updateGuild(data));
             });
-            dispatch(updateGuild(data));
-        });
 
-        return unsubscribe;
-    }, []);
+            return unsubscribe;
+        }
+    }, [id]);
 
     // Update messages when they change
     React.useEffect(() => {
-        const unsubscribe = onSnapshot(
-            collection(database, id, "data", "messages"),
-            (snapshot) => {
-                const messages: Record<string, MessageData> = {};
-                snapshot.docs.forEach((doc) => {
-                    messages[doc.id] = doc.data() as MessageData;
-                });
-                dispatch(updateData({ id: "messages", data: messages }));
-            }
-        );
-        return unsubscribe;
-    }, []);
+        if (id) {
+            const unsubscribe = onSnapshot(
+                collection(database, id, "data", "messages"),
+                (snapshot) => {
+                    const messages: Record<string, MessageData> = {};
+                    snapshot.docs.forEach((doc) => {
+                        messages[doc.id] = doc.data() as MessageData;
+                    });
+                    dispatch(updateData({ id: "messages", data: messages }));
+                }
+            );
+            return unsubscribe;
+        }
+    }, [id]);
 
     // Update events when they change
     React.useEffect(() => {
-        const unsubscribe = onSnapshot(
-            collection(database, id, "data", "events"),
-            (snapshot) => {
-                const events: Record<string, MessageData> = {};
-                snapshot.docs.forEach((doc) => {
-                    events[doc.id] = doc.data() as MessageData;
-                });
-                dispatch(updateData({ id: "events", data: events }));
-            }
-        );
-        return unsubscribe;
-    }, []);
+        if (id) {
+            const unsubscribe = onSnapshot(
+                collection(database, id, "data", "events"),
+                (snapshot) => {
+                    const events: Record<string, MessageData> = {};
+                    snapshot.docs.forEach((doc) => {
+                        events[doc.id] = doc.data() as MessageData;
+                    });
+                    dispatch(updateData({ id: "events", data: events }));
+                }
+            );
+            return unsubscribe;
+        }
+    }, [id]);
 
     // Update voicePresence when it changes
     React.useEffect(() => {
-        const unsubscribe = onSnapshot(
-            collection(database, id, "data", "voicePresence"),
-            (snapshot) => {
-                const voicePresence: Record<string, MessageData> = {};
-                snapshot.docs.forEach((doc) => {
-                    voicePresence[doc.id] = doc.data() as MessageData;
-                });
-                dispatch(
-                    updateData({ id: "voicePresence", data: voicePresence })
-                );
-            }
-        );
-        return unsubscribe;
-    }, []);
+        if (id) {
+            const unsubscribe = onSnapshot(
+                collection(database, id, "data", "voicePresence"),
+                (snapshot) => {
+                    const voicePresence: Record<string, MessageData> = {};
+                    snapshot.docs.forEach((doc) => {
+                        voicePresence[doc.id] = doc.data() as MessageData;
+                    });
+                    dispatch(
+                        updateData({ id: "voicePresence", data: voicePresence })
+                    );
+                }
+            );
+            return unsubscribe;
+        }
+    }, [id]);
 };
 
 export default useFirebaseListener;

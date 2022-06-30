a2ff43f1b7f5528518d8ff4816f5c6867bd9ace0
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-30 13:35:48 +0000

    feat: :sparkles: Add `addChannelToDatabase` function

---
+++ apps/bot/src/utils/function/database/addChannelToDatabase.ts
@@ -0,0 +1,38 @@
+import { TextChannel, VoiceChannel } from "discord.js";
+import DiscordClient from "../../../client/client";
+
+const addChannelToDatabase = async ({
+    client,
+    channel,
+}: {
+    client: DiscordClient;
+    channel: TextChannel | VoiceChannel;
+}) => {
+    const channelObject = {
+        id: channel.id,
+        name: channel.name,
+        type: channel.type,
+        guild: channel.guild.id
+    };
+
+    const { data: channelData, error } = await client.database
+        .from("channels")
+        .select()
+        .eq("id", channel.id);
+    if (error) console.log(error);
+
+    if (channelData?.length === 0) {
+        const { error } = await client.database
+            .from("channels")
+            .insert([channelObject]);
+        if (error) console.log(error);
+    } else {
+        const { error } = await client.database
+            .from("channels")
+            .update(channelObject)
+            .eq("id", channel.id);
+        if (error) console.log(error);
+    }
+};
+
+export { addChannelToDatabase };

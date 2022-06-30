870a4136cfce70cbc79166a497dc81e4fbcdce2b
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-30 13:38:10 +0000

    refactor: :card_file_box: Use supabase in `updateMessage` function

--- apps/bot/src/utils/function/updateMessage.ts
+++ apps/bot/src/utils/function/updateMessage.ts
@@ -1,61 +1,27 @@
-import { MessageData } from "@bienbot/types";
 import { Message } from "discord.js";
 import DiscordClient from "../../client/client";
+import { fetchMessage } from "./database/fetchMessage";
 
-const admin = require("firebase-admin");
-const database = admin.firestore();
-
-const parseAttachments = (message: Message) =>
-    JSON.parse(JSON.stringify([...message.attachments.values()]));
-
-export const updateMessage = async (
-    client: DiscordClient,
-    oldMessage: Message,
-    newMessage: Message
-) => {
+export const updateMessage = async ({
+    client,
+    oldMessage,
+    newMessage,
+}: {
+    client: DiscordClient;
+    oldMessage: Message;
+    newMessage: Message;
+}) => {
     const guildId = oldMessage?.guild?.id;
     if (!guildId) return;
 
-    // Find the message in the database
-    const messageChannelsRefs = await database
-        .collection(guildId)
-        .doc("messages")
-        .listCollections();
-
-    let message: MessageData | undefined;
-
-    for await (const channelRef of messageChannelsRefs) {
-        const channel = await channelRef.get();
-        channel.docs.forEach((doc: any) => {
-            const messageData = doc.data();
-            if (messageData.id === oldMessage.id) {
-                message = messageData;
-            }
-        });
-    }
-    if (!message) {
-        console.log("Edited message not found in database");
-        return;
-    }
-
-    // Change message content and history
-    message.content = {
-        text: newMessage.content,
-        attachments: parseAttachments(newMessage),
-    };
-    message.history = [
-        ...(message.history ?? []),
-        {
-            content: oldMessage.content,
-            attachments: parseAttachments(oldMessage),
-        },
-    ];
+    const messageData = await fetchMessage({ client, message: oldMessage });
+    messageData.content = newMessage.content;
+    messageData.history = [newMessage.content];
 
     // Update the message in the database
-    await database
-        .collection(guildId)
-        .doc("messages")
-        .collection(newMessage.channelId)
-        .doc(message.id)
-        .set(message);
+    const { error: updateError } = await client.database
+        .from("messages")
+        .update(messageData)
+        .eq("id", oldMessage.id);
+    if (updateError) console.log(updateError);
 };

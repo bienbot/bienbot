f79fc584d87c43721bee16a2d7c21c579afa0259
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-30 01:07:29 +0000

    feat: :card_file_box: Attatch database to DiscordClient

--- apps/bot/src/client/client.ts
+++ apps/bot/src/client/client.ts
@@ -2,11 +2,23 @@ import { Client, ClientOptions, Collection } from "discord.js";
 import BaseEvent from "../utils/structures/BaseEvent";
 import BaseCommand from "../utils/structures/BaseCommand";
 import { Player } from "discord-player";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+require("dotenv").config();
+
+const supabaseUrl = "https://hwljvreyautrqwyyassp.supabase.co";
+const supabaseKey = process.env["SUPABASE_SERVICE_ROLE"];
+if (!supabaseKey) {
+    throw new Error(
+        "No supabase key found. Please set the SUPABASE_SERVICE_ROLE environment variable."
+    );
+}
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default class DiscordClient extends Client {
     private _commands = new Collection<string, BaseCommand>();
     private _events = new Collection<string, BaseEvent>();
     private _prefix: string = "!";
+    private _database: SupabaseClient = supabase;
     private _player: Player = new Player(this, {
         ytdlOptions: {
             quality: "highestaudio",
@@ -41,5 +53,9 @@ export default class DiscordClient extends Client {
     }
     set prefix(prefix: string) {
         this._prefix = prefix;
+    }
+
+    get database(): SupabaseClient {
+        return this._database;
     }
 }

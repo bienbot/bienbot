8845408cc08bd9a23b2c5c0b4d0eccc9097c0b16
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-30 13:34:17 +0000

    feat: :sparkles: Add `addMessageToDatabase` function

---
+++ apps/bot/src/utils/function/database/addMessageToDatabase.ts
@@ -0,0 +1,32 @@
+import { Message } from "discord.js";
+import DiscordClient from "../../../client/client";
+
+const addMessageToDatabase = async ({
+    client,
+    message,
+}: {
+    client: DiscordClient;
+    message: Message;
+}) => {
+    const parsedAttachments = JSON.parse(
+        JSON.stringify([...message.attachments.values()])
+    );
+
+    const messageObject = {
+        id: message.id,
+        content: message.content,
+        timestamp: message.createdAt,
+        author: message.author.id,
+        channel: message.channel.id,
+        attachments: parsedAttachments ?? [],
+        guild: message.guild?.id,
+        deleted: false,
+    };
+
+    const { error } = await client.database
+        .from("messages")
+        .insert([messageObject]);
+    if (error) console.log(error);
+};
+
+export { addMessageToDatabase };

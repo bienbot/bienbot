948aea7c61c7c51251c16f9d34ef8418a882b693
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-06-30 13:38:54 +0000

    refactor: :card_file_box: Use supabase in bot events

--- apps/bot/src/events/join/JoinGuildEvent.ts
+++ apps/bot/src/events/join/JoinGuildEvent.ts
@@ -1,6 +1,5 @@
 import { Guild } from "discord.js";
 import DiscordClient from "../../client/client";
-import addToGuildsArray from "../../utils/function/addToGuildsArray";
 import BaseEvent from "../../utils/structures/BaseEvent";
 
 class JoinGuildEvent extends BaseEvent {
@@ -8,7 +7,11 @@ class JoinGuildEvent extends BaseEvent {
         super("guildCreate");
     }
     async run(client: DiscordClient, guild: Guild) {
-        addToGuildsArray(guild);
+        await client.database.from("guilds").insert({
+            id: guild.id,
+            name: guild.name,
+            reportChannelId: null,
+        });
     }
 }
 

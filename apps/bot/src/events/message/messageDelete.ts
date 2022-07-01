b8af463d21e61b5423ac2dd31242101de8974e72
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-07-01 07:34:33 +0000

    refactor: :card_file_box: Use supabase in `messageDelete` event handler

--- apps/bot/src/events/message/messageDelete.ts
+++ apps/bot/src/events/message/messageDelete.ts
@@ -1,7 +1,7 @@
 import BaseEvent from "../../utils/structures/BaseEvent";
 import { Message } from "discord.js";
 import DiscordClient from "../../client/client";
-import logEvent from "../../utils/function/logEvent";
+import { fetchMessage } from "../../utils/function/database/fetchMessage";
 
 class MessageEvent extends BaseEvent {
     constructor() {
@@ -13,16 +13,31 @@ class MessageEvent extends BaseEvent {
         const entry = logs?.entries?.first();
         if (!entry?.executor) return;
 
-        await logEvent({
-            eventDescription: "Deleted",
-            eventTarget: "message",
-            eventMember: entry.executor,
-            eventTargetId: message.id,
-            eventTime: new Date(),
-            guildId: message.guildId,
-            eventType: "messageDelete",
-            client,
-        });
+        /* Checking if the message is in the database, if it is, it will update the deleted column to
+       true. */
+        const databaseMessage = await fetchMessage({ client, message });
+        if (databaseMessage) {
+            await client.database
+                .from("messages")
+                .update({ deleted: true })
+                .eq("id", message.id);
+        }
+
+        /* Creating an object with the information of the event, and then inserting it into the
+        database. */
+        const eventObject = {
+            description: "Deleted",
+            target: "message",
+            targetId: message.id,
+            type: "messageDelete",
+            member: `${entry.executor.id}-${message.guild?.id}`,
+            guild: message.guild?.id,
+        };
+
+        const { error } = await client.database
+            .from("events")
+            .insert(eventObject);
+        if (error) console.log(error);
     }
 }
 

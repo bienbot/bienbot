8dfa133b1e7319acb1dc837dae1d3b881477f81d
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-07-01 07:33:18 +0000

    refactor: :card_file_box: Use supabase in `guildDelete` event handler

--- apps/bot/src/events/leave/LeaveGuildEvent.ts
+++ apps/bot/src/events/leave/LeaveGuildEvent.ts
@@ -1,6 +1,5 @@
 import { Guild } from "discord.js";
 import DiscordClient from "../../client/client";
-import removeFromGuildsArray from "../../utils/function/removeFromGuildsArray";
 import BaseEvent from "../../utils/structures/BaseEvent";
 
 class JoinGuildEvent extends BaseEvent {
@@ -8,7 +7,17 @@ class JoinGuildEvent extends BaseEvent {
         super("guildDelete");
     }
     async run(client: DiscordClient, guild: Guild) {
-        removeFromGuildsArray(guild);
+        /* Delete all the data from the database that is related to the guild that the bot left. */
+        await client.database.from("channels").delete().eq("guild", guild.id);
+        await client.database.from("events").delete().eq("guild", guild.id);
+        await client.database.from("guilds").delete().eq("id", guild.id);
+        await client.database.from("members").delete().eq("guild", guild.id);
+        await client.database.from("messages").delete().eq("guild", guild.id);
+        await client.database.from("roles").delete().eq("guild", guild.id);
+        await client.database
+            .from("voicePresences")
+            .delete()
+            .eq("guild", guild.id);
     }
 }
 

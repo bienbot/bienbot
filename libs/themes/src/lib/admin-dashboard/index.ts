3a73746f60a22288ae75134a5e6c749a8394023a
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-07-05 10:07:08 +0000

    feat: :label: Add interface for styled-components theme

--- libs/themes/src/lib/admin-dashboard/index.ts
+++ libs/themes/src/lib/admin-dashboard/index.ts
@@ -3,4 +3,43 @@
 import { DefaultTheme } from "styled-components";
 import theme from "./theme";
 
-export const dashboardTheme: DefaultTheme = theme;
+interface ITheme extends DefaultTheme {
+    font: {
+        family: string;
+        weight: {
+            regular: number;
+            medium: number;
+            semiBold: number;
+            bold: number;
+            extraBold: number;
+            black: number;
+        };
+        size: {
+            xsmall: string;
+            small: string;
+            medium: string;
+            large: string;
+            xlarge: string;
+        };
+    };
+    colors: {
+        primary: {
+            "100": string;
+            "200": string;
+            "300": string;
+            "400": string;
+            "500": string;
+            "600": string;
+            "700": string;
+            "800": string;
+            "900": string;
+            shadow: {
+                light: string;
+                strong: string;
+            };
+        };
+        background: string;
+    };
+}
+
+export const dashboardTheme: ITheme = theme;

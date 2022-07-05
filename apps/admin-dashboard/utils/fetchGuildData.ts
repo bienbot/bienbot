7f4e46f0f9104991d5b197d5a699fcbb2b1a1553
Łukasz Karasiński <56723259+lkarasinski@users.noreply.github.com>
2022-07-05 18:23:19 +0000

    perf: :zap: Use `Promise.all` instead of await

--- apps/admin-dashboard/utils/fetchGuildData.ts
+++ apps/admin-dashboard/utils/fetchGuildData.ts
@@ -1,46 +1,74 @@
 import { supabase } from "../services/supabase";
 
 const fetchGuildData = async ({ guildId }: { guildId: string }) => {
-    const { data: messagesData } = await supabase
-        .from(`messages`)
-        .select("*")
-        .match({ guild: guildId });
-
-    const { data: membersData } = await supabase
-        .from(`members`)
-        .select("*")
-        .match({ guild: guildId });
-
-    const { data: channelsData } = await supabase
-        .from(`channels`)
-        .select("*")
-        .match({ guild: guildId });
-
-    const { data: eventsData } = await supabase
-        .from(`events`)
-        .select("*")
-        .match({ guild: guildId });
-
-    const { data: voicePresencesData } = await supabase
-        .from(`voicePresences`)
-        .select("*")
-        .match({ guild: guildId });
-
-    const { data: serverInfoData } = await supabase
-        .from(`guilds`)
-        .select("*")
-        .match({ id: guildId })
-        .single();
-
     const guildData = {
-        name: serverInfoData.name,
+        name: "",
         id: guildId,
-        messages: messagesData,
-        members: membersData,
-        channels: channelsData,
-        events: eventsData,
-        voicePresences: voicePresencesData,
+        messages: [],
+        members: [],
+        channels: [],
+        events: [],
+        voicePresences: [],
     };
+
+    const fetchMessages = supabase
+        .from(`messages`)
+        .select()
+        .match({ guild: guildId })
+        .then(({ data }) => {
+            guildData.messages = data;
+        });
+
+    const fetchMembers = supabase
+        .from(`members`)
+        .select()
+        .match({ guild: guildId })
+        .then(({ data }) => {
+            guildData.members = data;
+        });
+
+    const fetchChannels = supabase
+        .from(`channels`)
+        .select()
+        .match({ guild: guildId })
+        .then(({ data }) => {
+            guildData.channels = data;
+        });
+
+    const fetchEvents = supabase
+        .from(`events`)
+        .select()
+        .match({ guild: guildId })
+        .then(({ data }) => {
+            guildData.events = data;
+        });
+
+    const fetchVoicePresences = supabase
+        .from(`voicePresences`)
+        .select()
+        .match({ guild: guildId })
+        .then(({ data }) => {
+            guildData.voicePresences = data;
+        });
+
+    const fetchGuildInfo = supabase
+        .from(`guilds`)
+        .select()
+        .match({ id: guildId })
+        .then(({ data }) => {
+            guildData.name = data[0].name;
+        });
+
+    const promises = [
+        fetchMessages,
+        fetchMembers,
+        fetchChannels,
+        fetchEvents,
+        fetchVoicePresences,
+        fetchGuildInfo,
+    ];
+
+    await Promise.all(promises);
 
     return guildData;
 };

9b8d9d097b679eabf854fe495f6ae6d37d690cdb
Mateusz Poniatowski <61357400+mattponiat@users.noreply.github.com>
2022-07-07 18:09:23 +0000

    feat: :sparkles: Add fuzzy search

--- apps/admin-dashboard/components/UsersDashboard/usersDashboard.style.tsx
+++ apps/admin-dashboard/components/UsersDashboard/usersDashboard.style.tsx
@@ -18,8 +18,9 @@ export const StyledUsersWrapper = styled.div`
 
 export const StyledUsersInfo = styled.div`
     display: flex;
-    align-items: center;
+    flex-direction: column;
     gap: 16px;
+    max-width: fit-content;
     margin-bottom: 32px;
 `;
 
@@ -32,6 +33,35 @@ export const StyledHeading = styled.h1`
     }
 `;
 
+export const StyledHeadingWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 16px;
+`;
+
 export const StyledSpan = styled.span`
     color: ${({ theme }) => theme.colors.primary[400]};
+`;
+
+export const StyledInput = styled.input`
+    width: 100%;
+    padding: 8px;
+    background-color: ${({ theme }) => theme.colors.primary[100]};
+    border: 2px solid ${({ theme }) => theme.colors.primary[300]};
+    border-radius: 8px;
+    color: ${({ theme }) => theme.colors.primary[500]};
+    font-family: ${({ theme }) => theme.font.family};
+    font-size: ${({ theme }) => theme.font.size.large};
+    outline: none;
+    transition: border-color 0.1s linear, box-shadow 0.1s linear;
+
+    :focus {
+        border-color: ${({ theme }) => theme.colors.primary[500]};
+        box-shadow: 0px 0px 2px 0.1px
+            ${({ theme }) => theme.colors.primary[500]};
+    }
+
+    ::placeholder {
+        color: ${({ theme }) => theme.colors.primary[300]};
+    }
 `;
